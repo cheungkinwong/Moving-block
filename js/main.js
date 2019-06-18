7117bfb4c6e309ba82f5dc7690de288aa328df61
cheungkinwong <cheungkin.wong@gmail.com>
2019-06-18 15:47:04 +0200

    pimp up the graphics

--- js/main.js
+++ js/main.js
@@ -1,21 +1,45 @@
-let box = document.getElementById("container");
-let pacman = document.getElementById("pacman");
+const box = document.getElementById("container");
+const pacman = document.getElementById("pacman");
+const counter = document.getElementById("score");
+
 let x = 0;
 let y = 0;
+let goal = 0;
 
 function spawntarget() {
+  let randomx = Math.floor(Math.random() * 101) * 5;
+  let randomy = Math.floor(Math.random() * 101) * 5;
   let spawn = document.createElement("div");
-  let random = Math.floor(Math.random() * 101);
   spawn.id = "target";
-  spawn.style.left = random + "%";
-  spawn.style.top = random + "%";
+  spawn.innerHTML = '<i class="fas fa-globe-asia"></i>';
+  spawn.style.left = randomx + "px";
+  spawn.style.top = randomy + "px";
   box.appendChild(spawn);
 }
 spawntarget();
 
+function collision() {
+  let spawnx = document.getElementById("target").offsetLeft;
+  let spawny = document.getElementById("target").offsetTop;
+  let pacx = document.getElementById("pacman").offsetLeft;
+  let pacy = document.getElementById("pacman").offsetTop;
+  let distantx = parseInt(spawnx) - parseInt(pacx);
+  let distanty = parseInt(spawny) - parseInt(pacy);
+
+  if (distantx === 0 && distanty === 0) {
+    goal += 1;
+    let target = document.getElementById("target");
+    counter.innerHTML = goal;
+    target.remove();
+    spawntarget();
+  }
+  console.log(spawnx, spawny);
+}
+
 document.addEventListener("keydown", move);
 
 function move(event) {
+  collision();
   switch (event.keyCode) {
     case 37:
       left();
@@ -35,20 +59,20 @@ function move(event) {
 function left() {
   x -= 5;
   pacman.style.left = x + "px";
-  console.log("left");
+  pacman.innerHTML = '<i class="fas fa-space-shuttle fa-flip-horizontal" >';
 }
 function right() {
   x += 5;
   pacman.style.left = x + "px";
-  console.log("right");
+  pacman.innerHTML = '<i class="fas fa-space-shuttle">';
 }
 function up() {
   y -= 5;
   pacman.style.top = y + "px";
-  console.log("up");
+  pacman.innerHTML = '<i class="fas fa-space-shuttle fa-rotate-270" >';
 }
 function down() {
   y += 5;
   pacman.style.top = y + "px";
-  console.log("down");
+  pacman.innerHTML = '<i class="fas fa-space-shuttle fa-rotate-90" >';
 }
