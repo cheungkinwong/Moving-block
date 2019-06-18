ad5d05933c9e4621a6c5e1000c68afc9b6fefc95
cheungkinwong <cheungkin.wong@gmail.com>
2019-06-18 11:00:23 +0200

    add spawntarget function

--- js/main.js
+++ js/main.js
@@ -1,51 +1,54 @@
+let box = document.getElementById("container");
 let pacman = document.getElementById("pacman");
-let pos = 0;
+let x = 0;
+let y = 0;
+
+function spawntarget() {
+  let spawn = document.createElement("div");
+  let random = Math.floor(Math.random() * 101);
+  spawn.id = "target";
+  spawn.style.left = random + "%";
+  spawn.style.top = random + "%";
+  box.appendChild(spawn);
+}
+spawntarget();
 
 document.addEventListener("keydown", move);
-
-function left() {
-  pos -= 5;
-  pacman.style.left = pos + "px";
-  console.log("left");
-}
-function right() {
-  pos += 5;
-  pacman.style.left = pos + "px";
-  console.log("right");
-}
-function up() {
-  pos -= 5;
-  pacman.style.top = pos + "px";
-  console.log("up");
-}
-function down() {
-  pos += 5;
-  pacman.style.top = pos + "px";
-  console.log("down");
-}
 
 function move(event) {
   switch (event.keyCode) {
     case 37:
       left();
       break;
-
     case 39:
       right();
       break;
-
     case 38:
       up();
       break;
-
     case 40:
       down();
       break;
   }
 }
 
-function gameLoop() {
-  move();
-  setTimeout("gameLoop()", 10);
+function left() {
+  x -= 5;
+  pacman.style.left = x + "px";
+  console.log("left");
 }
-gameLoop();
+function right() {
+  x += 5;
+  pacman.style.left = x + "px";
+  console.log("right");
+}
+function up() {
+  y -= 5;
+  pacman.style.top = y + "px";
+  console.log("up");
+}
+function down() {
+  y += 5;
+  pacman.style.top = y + "px";
+  console.log("down");
+}
